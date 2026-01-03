69489ae9d8264752da0601b5bec0db1f4bbb7e9c
RautRitesh <ritesh208318@gmail.com>
2026-01-03 20:13:20 +0545

    feat/added all the resume models

--- applicants/models.py
+++ applicants/models.py
@@ -10,4 +10,21 @@ class ResumeAnalysis(BaseModel):
         related_name='resume_analysis'
     )
     job_title=models.CharField(max_length=255,help_text="Target job Role")
+    job_description=models.TextField(help_text="The raw requirements for the jobs")
+    resume_file=models.FileField(upload_to='resume/pdf/')
+    overall_match_score=models.FloatField(default=0.0)
+    section_match_score=models.JSONField(default=dict)
+    retrieved_evidence=models.JSONField(default=dict,blank=True)
+    missing_keywords=models.JSONField(default=list,blank=True)
+    analysis_summary=models.TextField(blank=True,help_text="AI generated summary for the resume")
+    improved_suggestion=models.JSONField(default=list,blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}|{self.job_title}"
+    
+    class Meta:
+        ordering=['-created_at']
+        verbose_name_plural="Resume Analyses"
+        
+    
     
